85d053efdfb6270ec0bef06e5ab0f628f238d4da
Omar Harradi <oharradi.ieu2024@student.ie.edu>
2025-02-03 20:40:41 +0100

    Update Chat.tsx

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -71,7 +71,7 @@ Your task is to analyze the latest BTC-USD market trends based on price movement
     `;
 
     try {
-      const res = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBvY2E5iSzi9M_O8GB6fgxmb3Dy9gvREKo", {
+      const res = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=API_KEY", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
